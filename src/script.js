ac0a5ca3225ac3b4239ffe22d9d39b4d89e79527
Brandon Tsang <brandononline2@gmail.com>
2021-07-24 17:08:00 -0400

    add prettier config

--- src/script.js
+++ src/script.js
@@ -1,32 +1,39 @@
 async function getWeather(lat, lon) {
-    const url = `http://api.weatherapi.com/v1/current.json?key=bbcd6df96dd44e51bba201926212207&q=${lat},${lon}`
+  const url = `http://api.weatherapi.com/v1/current.json?key=bbcd6df96dd44e51bba201926212207&q=${lat},${lon}`;
+  const result = await (await fetch(url)).json();
+
+  {
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=bbcd6df96dd44e51bba201926212207&q=${lat},${lon}&days=5`;
     const result = await (await fetch(url)).json();
-    return result;
+    console.log(result);
+  }
+
+  return result;
 }
 
 async function updateWeather(weather) {
-    document.querySelector('#temp').textContent = weather.current.temp_f;
-    document.querySelector('#location').textContent = weather.location.name;
-    document.querySelector('#country').textContent = weather.location.country;
-    document.querySelector('#condition-icon').src = weather.current.condition.icon;
+  document.querySelector("#temp").textContent = weather.current.temp_f;
+  document.querySelector("#location").textContent = weather.location.name;
+  document.querySelector("#country").textContent = weather.location.country;
+  document.querySelector("#condition-icon").src = weather.current.condition.icon;
 }
 
-document.querySelector('#form').addEventListener('submit', async (evt) => {
-    evt.preventDefault();
+document.querySelector("#form").addEventListener("submit", async (evt) => {
+  evt.preventDefault();
 
-    const lat = document.querySelector('#lat').value || 0;
-    const lon = document.querySelector('#lon').value || 0;
-    const weather = await getWeather(lat, lon);
-    updateWeather(weather);
-})
+  const lat = document.querySelector("#lat").value || 0;
+  const lon = document.querySelector("#lon").value || 0;
+  const weather = await getWeather(lat, lon);
+  updateWeather(weather);
+});
 
 async function getUserDeviceLocation() {
-    const geoLoc = navigator.geolocation;
-    geoLoc.getCurrentPosition(async (pos) => {
-        const coordinates = pos.coords;
-        const weather = await getWeather(coordinates.latitude, coordinates.longitude);
-        updateWeather(weather);
-    });
+  const geoLoc = navigator.geolocation;
+  geoLoc.getCurrentPosition(async (pos) => {
+    const coordinates = pos.coords;
+    const weather = await getWeather(coordinates.latitude, coordinates.longitude);
+    updateWeather(weather);
+  });
 }
 
 getUserDeviceLocation();
